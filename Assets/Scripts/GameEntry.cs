8cbd987fa51472fe246aaa0ba36a3a085d51cc03
guanjinbiao <guanjinbiao@gyyx.cn>
2022-04-15 15:24:49 +0800

    + hex map sample

---
+++ Assets/Scripts/GameEntry.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HexMap;
+
+public class GameEntry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnEnable()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnSceneLoaded(Scene scene, object data)
+    {
+    }
+}
